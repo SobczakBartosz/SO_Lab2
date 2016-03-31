043010d4880c2d1273f474fc5699cd76b01c72d0
Agnieszka <agulec113@wp.pl>
2016-03-31 22:17:04 +0200

    Create Request class

--- src/Request.java
+++ src/Request.java
@@ -1,4 +1,31 @@
 
 public class Request {
+	
+	private final int track;
+	private boolean deadline;
+	private boolean isDone;
+	
+	public Request(int track, boolean deadline) {
+		this.track = track;
+		this.deadline = deadline;
+		isDone = false;
+	}
 
+	public boolean isDone() {
+		return isDone;
+	}
+
+	public void setDone(boolean isDone) {
+		this.isDone = isDone;
+	}
+
+	public int getTrack() {
+		return track;
+	}
+
+	public boolean isDeadline() {
+		return deadline;
+	}
+	
+	
 }
