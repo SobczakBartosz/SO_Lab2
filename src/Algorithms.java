2adc83e0a4dd66185e8175edab35c7290ed99d8b
SobczakBartosz <bartoszsobczak95@gmail.com>
2016-04-04 23:25:03 +0200

    Create RequestComparator, modify Disc class, create scan algorithm

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,4 +1,31 @@
+import java.util.*;
 
 public class Algorithms {
+	
+	public int scan(Disc disc) {
+		int result = 0;
+		int index;
+		Request current = null;
+		Request tmp = null;
+		ArrayList<Request> requestsClone = disc.clone();
+		
+		Collections.sort(requestsClone, new RequestComparator());
+		index = requestsClone.size()/2;
+		current = requestsClone.get(index);
 
+//		Iterator it = requestsClone.iterator();
+		
+		while(index < requestsClone.size()-1) {
+			tmp = requestsClone.get(index+1);
+			result += tmp.getTrack() - current.getTrack();
+			requestsClone.remove(current);
+			current = tmp;	
+		}
+		while(requestsClone.size() <= 1) {
+			result += requestsClone.get(index-1).getTrack();
+			requestsClone.remove(index);
+		}
+
+		return result;
+	}
 }
--- src/Disc.java
+++ src/Disc.java
@@ -1,10 +1,9 @@
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
 
 public class Disc {
 
 	private final int MAX_TRACK;
-	ArrayList<Request> disc = new ArrayList<Request>();
+	ArrayList<Request> requests = new ArrayList<Request>();
 	
 	public Disc(int max, int numberOfRequests, boolean deadline) {
 		Random rand = new Random();
@@ -12,11 +11,16 @@ public class Disc {
 		
 		if(deadline == false) {
 			for(int i = 0; i < numberOfRequests; i++)
-				disc.add(new Request(rand.nextInt(MAX_TRACK+1),false));			
+				requests.add(new Request(rand.nextInt(MAX_TRACK+1),false));			
 		} else {
 			for(int i = 0; i < numberOfRequests; i++)
-				disc.add(new Request(rand.nextInt(MAX_TRACK+1),rand.nextBoolean()));
+				requests.add(new Request(rand.nextInt(MAX_TRACK+1),rand.nextBoolean()));
 		}	
 	} // constructor
 	
+	public ArrayList<Request> clone() {
+		ArrayList<Request> cloneArray = new ArrayList<Request>();
+			cloneArray.addAll(requests);
+		return cloneArray;
+	}
 }
