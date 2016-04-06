1336831c22d467b4a4f482b1bf99f61ce5d156c2
SobczakBartosz <bartoszsobczak95@gmail.com>
2016-04-07 00:11:59 +0200

    Modify EDF and implement EDF in other algorithms

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -2,26 +2,33 @@ import java.util.*;
 
 public class Algorithms {
 	
-	public int FCFS(Disc disc) {
+	public int FCFS(Disc disc, boolean real_time) {
 		int result = 0;
 		ArrayList<Request> requestsClone = disc.clone();
 		
+		if(real_time == true) {
+			result += EDF(requestsClone);
+		}	
+		
 		while(requestsClone.size() > 1) {
-			if(requestsClone.get(1).getTrack() > requestsClone.get(0).getTrack())
-				result += requestsClone.get(1).getTrack() - requestsClone.get(0).getTrack();
-			else
-				result += requestsClone.get(0).getTrack() - requestsClone.get(1).getTrack();
+			result += Math.abs(requestsClone.get(1).getTrack() - requestsClone.get(0).getTrack());
 			requestsClone.remove(0);
 		}
 		return result;
 	} // FCFS algorithms
 	
-	public int SSTF(Disc disc) {
+	public int SSTF(Disc disc, boolean real_time) {
+		int index;
 		int result = 0;
+		Request first;
 		ArrayList<Request> requestsClone = disc.clone();
 		
+		if(real_time == true)
+			result += EDF(requestsClone);
+		
+		first = requestsClone.get(0);
 		Collections.sort(requestsClone, new RequestComparator()); //sort by track
-		int index = requestsClone.size()/2;
+		index = requestsClone.indexOf(first);
 		
 		while(requestsClone.size() > 1) {
 			if(index == 0) {
@@ -45,12 +52,19 @@ public class Algorithms {
 		return result;
 	} // SSTF algorithm
 	
-	public int scan(Disc disc) {
+	public int scan(Disc disc, boolean real_time) {
+		int index;
 		int result = 0;
+		Request first;
 		ArrayList<Request> requestsClone = disc.clone();
 		
+		if(real_time == true)
+			result += EDF(requestsClone);
+		
+		first = requestsClone.get(0);
 		Collections.sort(requestsClone, new RequestComparator()); //sort by track
-		int index = requestsClone.size()/2;
+		index = requestsClone.indexOf(first);
+
 		
 		while(index < requestsClone.size()-1) {
 			result += requestsClone.get(index+1).getTrack() - requestsClone.get(index).getTrack();
@@ -68,12 +82,18 @@ public class Algorithms {
 		return result;
 	} // SCAN algorithm
 	
-	public int cscan(Disc disc) {
+	public int cscan(Disc disc, boolean real_time) {
+		int index;
 		int result = 0;
+		Request first;
 		ArrayList<Request> requestsClone = disc.clone();
 		
+		if(real_time == true)
+			result += EDF(requestsClone);
+		
+		first = requestsClone.get(0);
 		Collections.sort(requestsClone, new RequestComparator()); //sort by track
-		int index = requestsClone.size()/2;
+		index = requestsClone.indexOf(first);
 		
 		while(index < requestsClone.size()-1) {
 			result += requestsClone.get(index+1).getTrack() - requestsClone.get(index).getTrack();
@@ -87,20 +107,30 @@ public class Algorithms {
 		return result;
 	} // C-SCAN algorithm
 	
-	public int EDF(Disc disc) {
+	public int EDF(ArrayList<Request> requests) {
 		int result = 0;
-		ArrayList<Request> requestsClone = disc.clone();
+		int current = 0;
+		int index = 1;
+
+		Collections.sort(requests, new RequestComparator2()); //sort by deadline
+		Collections.reverse(requests);
+		requests.get(current).setDeadline(0);
 		
-		Collections.sort(requestsClone, new RequestComparator2()); //sort by deadline
-		
-		while(requestsClone.size() > 1) {
-			if(requestsClone.get(1).getTrack() - requestsClone.get(0).getTrack() > 0)
-				result += (requestsClone.get(1).getTrack() - requestsClone.get(0).getTrack());
-			else
-				result += (requestsClone.get(0).getTrack() - requestsClone.get(1).getTrack());
-			requestsClone.remove(0);
+		while(requests.get(index).isPriority() == true) {
+			if(requests.get(index).getDeadline() > Math.abs(requests.get(index).getTrack() - requests.get(current).getTrack())) {
+				result += Math.abs(requests.get(index).getTrack() - requests.get(current).getTrack());
+				requests.remove(current);
+				current = index;
+				
+			} else
+				requests.get(index).setDeadline(0);
+			index++;
 		}
+		Request tmp = requests.get(0);
+		requests.set(0, requests.get(current));
+		requests.set(current, tmp);
+		
 		return result;
-	} // EDF algorithm (use real-time requests)
-	
+	} //EDF algorithm for handling real-time requests
+
 }
