431443a2d85391446b0484b59f71b11f6e023cdc
Agnieszka <agulec113@wp.pl>
2016-04-06 18:37:52 +0200

    Create SSTF algorithm

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -11,7 +11,48 @@ public class Algorithms {
 			requestsClone.remove(0);
 		}
 		return result;
-	}
+	} // FCFS algorithms
+	
+	public int SSTF(Disc disc) {
+		int index;
+		int result = 0;
+		Request current = null;
+		Request tmp = null;
+		ArrayList<Request> requestsClone = disc.clone();
+		
+		Collections.sort(requestsClone, new RequestComparator()); //sort by track
+		index = requestsClone.size()/2;
+		current = requestsClone.get(index);
+		
+		while(requestsClone.size() > 1) {
+			if(index == 0) {
+				tmp = requestsClone.get(1);
+				result += current.getTrack() - tmp.getTrack();
+				requestsClone.remove(current);
+				current = tmp;
+			} else if(index == requestsClone.size()-1) {
+				tmp = requestsClone.get(requestsClone.size()-2);
+				result += current.getTrack() - tmp.getTrack();
+				requestsClone.remove(current);
+				current = tmp;
+				index--;
+			} else {
+				if(current.getTrack() - requestsClone.get(index-1).getTrack() > requestsClone.get(index+1).getTrack() - current.getTrack()) {
+				tmp = requestsClone.get(index-1);
+				result += current.getTrack() - tmp.getTrack();
+				requestsClone.remove(current);
+				current = tmp;
+				index--;
+				} else {
+					tmp = requestsClone.get(index+1);
+					result += tmp.getTrack() - current.getTrack();
+					requestsClone.remove(current);
+					current = tmp;
+				}
+			}
+		}
+		return result;
+	} // SSTF algorithm
 	
 	public int scan(Disc disc) {
 		int index;
